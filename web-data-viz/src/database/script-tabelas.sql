cdc48c791a168e271b23ae399a371c1807e571ee
IlysMaroto <ilys.maroto@sptech.school>
2024-06-10 11:18:43 -0300

    Alterações finais - revisar

--- web-data-viz/src/database/script-tabelas.sql
+++ web-data-viz/src/database/script-tabelas.sql
@@ -1,59 +1,64 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+-- NOME: ILYS GOMES MAROTO
+-- RA: 04241067
+-- CRIAÇÃO DO BANCO DE DADOS PARA O PROJETO INDIVIDUAL
 
-/*
-comandos para mysql server
-*/
+DROP DATABASE IF EXISTS 39TALK;
+CREATE DATABASE 39TALK;
+USE 39TALK;
 
-CREATE DATABASE aquatech;
-
-USE aquatech;
-
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14)
+CREATE TABLE VOCALOIDFAVORITO (
+  IDVOCALOID INT PRIMARY KEY auto_increment,
+  NOME VARCHAR(256)
 );
 
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+CREATE TABLE IMAGEMPERFIL (
+  IDIMAGEM INT PRIMARY KEY auto_increment,
+  IMAGEM VARCHAR(256)
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+CREATE TABLE USUARIO(
+  IDUSUARIO INT AUTO_INCREMENT,
+  NOME VARCHAR(256),
+  EMAIL VARCHAR(256) UNIQUE,
+  SENHA VARCHAR(256),
+  FKVOCALOID INT,
+  FKIMAGEM INT,
+  GENERO VARCHAR(256),
+
+  PRIMARY KEY (IDUSUARIO, FKIMAGEM, FKVOCALOID),
+  FOREIGN KEY (FKIMAGEM) REFERENCES IMAGEMPERFIL(IDIMAGEM),
+  FOREIGN KEY (FKVOCALOID) REFERENCES VOCALOIDFAVORITO(IDVOCALOID)
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+CREATE TABLE JOGO(
+  IDJOGO INT PRIMARY KEY AUTO_INCREMENT,
+  NOME VARCHAR(256)
 );
 
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
+CREATE TABLE PONTUACAO (
+  IDPONTUACAO INT AUTO_INCREMENT,
+  FKJOGO INT,
+  FKUSUARIO INT,
+  PONTUACAO INT,
+  VEZESJOGADAS INT,
 
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
+  PRIMARY KEY (IDPONTUACAO, FKJOGO, FKUSUARIO),
+  FOREIGN KEY (FKJOGO) REFERENCES JOGO(IDJOGO),
+  FOREIGN KEY (FKUSUARIO) REFERENCES USUARIO(IDUSUARIO)
 );
 
-insert into empresa (razao_social, cnpj) values ('Empresa 1', '00000000000000');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
+INSERT INTO VOCALOIDFAVORITO (NOME) VALUES
+  ('Hatsune Miku'),
+  ('Kaito'),
+  ('Meiko'),
+  ('Megurine Luka'),
+  ('Kagamine Len'),
+  ('Kagamine Rin');
+
+INSERT INTO IMAGEMPERFIL (IMAGEM) VALUES
+  ('../assets/icon/mikuicon.jpg'),
+  ('../assets/icon/kaitoicon.jpg'),
+  ('../assets/icon/meikoicon.jpg'),
+  ('../assets/icon/lukaicon.jpg'),
+  ('../assets/icon/lenicon.jpg'),
+  ('../assets/icon/rinicon.jpg');
--- web-data-viz/src/models/usuarioModel.js
+++ web-data-viz/src/models/usuarioModel.js
@@ -1,22 +1,29 @@
 var database = require("../database/config")
 
-function autenticar(email, senha) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+// Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
+function cadastrar(nome, email, senha, genero, vocaloid) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
+    
+    // são 2 inserts
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instruSelVocal = `
+    `
+    // Fazer um select do vocaloidId
+    console.log("LALAL")
+
     var instrucaoSql = `
-        SELECT id, nome, email, fk_empresa as empresaId FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+        INSERT INTO USUARIO (NOME, EMAIL, SENHA, GENERO, FKVOCALOID, FKIMAGEM) VALUES ('${nome}', '${email}', '${senha}', '${genero}', '${vocaloid}', null);
     `;
+
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
-// Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
-function cadastrar(nome, email, senha, empresaId) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha);
-    
-    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
-    //  e na ordem de inserção dos dados.
+function autenticar(email, senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucaoSql = `
-        INSERT INTO usuario (nome, email, senha, fk_empresa) VALUES ('${nome}', '${email}', '${senha}', '${empresaId}');
+        SELECT IDUSUARIO as idusuario, NOME as nome, EMAIL as email, SENHA as senha FROM USUARIO WHERE email = '${email}' AND senha = '${senha}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
